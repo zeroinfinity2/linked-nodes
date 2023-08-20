eeba014a4b7d639c58bd2b333c74b67dac6134f3
zeroinfinity2 <zeroinfinity86@gmail.com>
2023-08-20 14:21:22 -0400

    style changes

--- node.py
+++ node.py
@@ -4,6 +4,7 @@ Part of the graph traversal algorithm using connected nodes prototype.
 '''
 import math
 
+
 class Node:
     '''
     Identifies the node class and all node operations.
@@ -17,7 +18,7 @@ class Node:
         self.node_type = node_type
 
         # Set node_pos[x,y] to 0,0 if not defined
-        self.node_pos = node_pos if node_pos is not None else [0,0]
+        self.node_pos = node_pos if node_pos is not None else [0, 0]
         self.connected_nodes = connected_nodes
 
         # Sets node initial state to 'init'
@@ -29,25 +30,22 @@ class Node:
         # Node's score is set to an initial value
         self.score = 0
 
-
     def open_node(self, opening_node):
         ''' Set a state to be open, ready for evaluating '''
         self.state = 'open'
         # Sets previous_node to the node that opened it
         self.previous_node = opening_node
 
-
     def close_node(self):
         ''' Set a state to closed, after evaluation has completed '''
         self.state = 'closed'
         self.score = 0
 
-
     def score_node(self, endnode, previous_node):
         '''
         Score the node based on the following formula:
         dist_to_end_node + dist_to_previous_node + previous_node_score
-        This heuristic will probably change. 
+        This heuristic will probably change.
         '''
         dt_endnode = math.dist(endnode.node_pos, self.node_pos)
         dt_previous = math.dist(previous_node.node_pos, self.node_pos)
